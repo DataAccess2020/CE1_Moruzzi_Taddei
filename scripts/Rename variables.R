caf488ae93ecbf11bd9cc39455b4d3a2e81039a5
federico00taddei <icotaddei@gmail.com>
2022-11-26 18:51:43 +0100

    Dataset processed exoprt

--- scripts/Rename variables.R
+++ scripts/Rename variables.R
@@ -5,3 +5,9 @@ income <- h_i_rec$v261_r
 
 #Create a dataframe with the two variables (taddei)
 he_in_df <- data.frame(health, income)
+
+
+#export dataset
+write.csv(h_i_rec,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CLASS EXCERCISE\\CE1_Moruzzi_Taddei\\data_proc\\evs_recoded.csv", row.names = FALSE)
+
+write.csv(he_in_df,"C:\\Users\\39333\\Desktop\\UNIVERSITA\\MAGISTRALE COM-DAPS&CO\\CORSI\\DATA ACCESS AND REGULATIONS\\CLASS EXCERCISE\\CE1_Moruzzi_Taddei\\data_proc\\df_health_income.csv", row.names = FALSE)
