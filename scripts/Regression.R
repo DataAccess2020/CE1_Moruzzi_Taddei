0b36abd10061f23127b7b4fa3a38d61b617574d2
chiaramoruzzi <chiaramoruzzi@gmail.com>
2022-11-26 18:48:39 +0100

    regression + comments

---
+++ scripts/Regression.R
@@ -0,0 +1,17 @@
+#REGRESSION
+
+reg <- lm(health ~ income,
+          data = he_in_df)
+
+summary(reg)
+
+# intercept = 3.2067
+# coefficient = 0.2568
+# adjusted R-squared = 0.0494
+# *** level of significance
+
+
+
+
+
+
