296caef835cbaf4b02266e8ad171f7a5d50f49db
chiaramoruzzi <chiaramoruzzi@gmail.com>
2022-11-27 11:42:13 +0100

    literature notes _ 1

---
+++ Notes_literature.R
@@ -0,0 +1,122 @@
+# NOTES ON LITERATURE REGARDING HEALTH AND INCOME
+
+
+#https://doi.org/10.1377/hlthaff.21.2.31
+#
+# there are two ways in which income could be causally related to health
+# 1. direct effect on the material conditions necessary for survival 
+# 2. effect on social participation and opportunity to control life circumstances
+#
+#
+# 1
+# there are a set of material conditions that are necessary for good health:
+# clean water, good sanitation, adequate nutrition, adequate housing and warmth
+#
+# these material conditions are correlated with income until a certain threshold
+# is reached: below the threshold, the lower income, the worse the health because
+# of the link with material conditions. Above the threshold level, differences in material
+# conditions no longer have any plausible connection with differences in pathology.
+#
+# For people above the threshold there could still be substantial inequalities
+# in health that are related to differing opportunities for social participation,
+# for leading a fulfilling and satisfying life, and for control over one's life.
+# Depending on how society was organized, these opportunities could show
+# a strong direct link with individual income. In this case income would be 
+# correlated with health, not through material conditions, but rather through 
+# social participation
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
