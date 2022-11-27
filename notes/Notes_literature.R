ee13fe2764fba07b8bcc694242278ab4f9172d6d
chiaramoruzzi <chiaramoruzzi@gmail.com>
2022-11-27 16:04:33 +0100

    puts three files in the new folder "notes"

---
+++ notes/Notes_literature.R
@@ -0,0 +1,162 @@
+# NOTES ON LITERATURE REGARDING HEALTH AND INCOME
+
+
+#https://doi.org/10.1377/hlthaff.21.2.31
+#
+# there are two ways in which income could be causally related to health
+# 1. direct effect on the material conditions necessary for survival 
+# 2. effect on social participation and opportunity to control life circumstances
+#
+#
+# 1
+# there are a set of material conditions that are necessary for good health:
+# clean water, good sanitation, adequate nutrition, adequate housing and warmth
+#
+# these material conditions are correlated with income until a certain threshold
+# is reached: below the threshold, the lower income, the worse the health because
+# of the link with material conditions. Above the threshold level, differences in material
+# conditions no longer have any plausible connection with differences in pathology.
+#
+# For people above the threshold there could still be substantial inequalities
+# in health that are related to differing opportunities for social participation,
+# for leading a fulfilling and satisfying life, and for control over one's life.
+# Depending on how society was organized, these opportunities could show
+# a strong direct link with individual income. In this case income would be 
+# correlated with health, not through material conditions, but rather through 
+# social participation
+#
+# the author suggests that if we compare rich countries and poor countries the results
+# might change a little also due to the intercation of the education factor
+#
+# moreover, income many times determines the ability to participate in the way defined
+# as acceptable by society
+#
+# The higher the income, the more likely people are to report themselves in 
+# good health and the less likely to report depression. Income is only one measure 
+# of social position: In Whitehall II the most powerful predictor has always been 
+# position in the occupational hierarchy
+#
+# A tentative conclusion is that in a population above the poverty level, 
+# income is important as a predictor of ill health because it is a measure 
+# of where a person is in the social hierarchy, rather than because of pounds, 
+# dollars, or euros in the pocket
+#
+
+
+
+
+
+#https://www.furniturebank.org/wp-content/uploads/2016_Social-Determinants-of-Health-Housing-and-Income.pdf
+#
+# When speaking of health, it's important to point out that there are many dimension
+# of this concept apart from the general one. For example, mental health is a big
+# part of what can be considered as "general health". Considering this assumption makes it 
+# easier to understand why social inclusion and participation is an important
+# element for health.
+
+
+# Having a stable income, either from employment or through financial support 
+# and benefits, is directly related to other determinants of health, namely
+# affordable housing andrent, food and transportation to access healthcare
+# Social inclusion, or the ability to engage fully in the community,
+# is also somewhat dependent on available finances
+#
+#
+#
+#
+
+
+
+
+
+# DOI: 10.1146/annurev.publhealth.23.100901.140540
+# THE MACROECONOMIC DETERMINANTS OF HEALTH
+
+
+#The possibility has therefore been raised that the ecological relationship 
+# between income inequality and poor health is not universal, but rather depends 
+# on the social policies that governments deploy to mitigate the effects of 
+# economic inequalities
+
+
+# At present, three separate, though not mutually exclusive, mechanisms have 
+# been put forward.
+# The first is through reduced access to life opportunities, material resources, 
+# and opportunity structures. The second set of processes is linked to decreasing 
+# social capital that manifests through factors such as erosion of social cohesion, 
+# increased social exclusion, and conflict. Third, more direct psycho-social 
+# pathways such as hopelessness, lack of control, or loss of respect arising as 
+# a consequence of inequality have been identified as having a potential effect 
+# on individual health
+
+
+# The evidence is now overwhelming that poverty (whether conceptualized at the
+# country level or at the individual level) is strongly correlated with poorer 
+# health outcomes, on average. Moreover, the relationship is likely reciprocal 
+# and reinforcing, in that societies burdened with diseases, suffering from 
+# endemic diseases, and with lower life expectancies tend to experience low 
+# productivity and low and unstable economic growth.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
